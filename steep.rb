40b8af4609cee79f4a5a972c97e8e24d69124183
Chris Hernandez <c.scott.hernandez@gmail.com>
2011-06-21 14:47:38 -0400

    changed percentage animation; end ascii art

--- steep.rb
+++ steep.rb
@@ -6,18 +6,16 @@ require 'optparse'
 
 class Steeper
   
+  ColumnWidth = 60
   ColorMap = [
     "\033[0;37;40m",
     "\033[1;30;40m",
     "\033[0;33;40m",
     "\033[1;33;40m",
     "\033[1;32;40m",
-    "\033[0;32;40m",
-    "\033[0;31;40m",
-    "\033[1;31;40m"
+    "\033[0;32;40m"
     ]
-  
-  DoneMessage = "        {\n     {   }\n      }_{ __{\n   .-{   }   }-.\n  (   }     {   )\n  |`-.._____..-'|\n  |             ;--.\n  |            (__  \\\n  |             | )  )\n  |             |/  /\n  |             /  /\n  |            (  /\n  \             y'\n   `-.._____..-'\n"
+  DoneMessage = "\t\t        {\n\t\t     {   }\n\t\t      }_{ __{\n\t\t   .-{   }   }-.\n\t\t  (   }     {   )\n\t\t  |`-.._____..-'|\n\t\t  |             ;--.\n\t\t  |            (__  \\\n\t\t  |             | )  )\n\t\t  |             |/  /\n\t\t  |             /  /\n\t\t  |            (  /\n\t\t  \\             y'\n\t\t   `-.._____..-'\n"
   
   def initialize
     
@@ -25,7 +23,7 @@ class Steeper
     # for configurations, times, etc...
     # also, this will check for past steepings to
     # keep track for multiple steepings
-    @steep_duration = 55 # in seconds
+    @steep_duration = 2 # in seconds
     
     # parsing command line options
     @options = {}
@@ -45,12 +43,10 @@ class Steeper
     
     until Time.now.to_f > stop_time
       percent =  (Time.now.to_f - start_time) / @steep_duration.to_f
-      print "#{"#{ColorMap[percent * 6]}#"*(percent * 50)}\r"
+      print "#{"#{ColorMap[percent * 6]}#"*(percent * ColumnWidth)}\r"
       STDOUT.flush
     end
-    print DoneMessage
-    print "\033[0m\n"
-    puts "done steeping"
+    puts "#{" "*ColumnWidth}\n#{"\007"*3}#{DoneMessage}\033[0m\ndone steeping"
   end
     
 end
