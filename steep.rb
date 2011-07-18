751f95c0df1bbe918a1b5cd59c2932487ad7a0aa
Chris Hernandez <c.scott.hernandez@gmail.com>
2011-07-18 13:06:50 -0400

    restructured to be more oriented around class

--- steep.rb
+++ steep.rb
@@ -7,7 +7,7 @@ require 'yaml'
 
 class Steeper
   
-  ConfigFile = "steep.yml"
+  ConfigFile = File.join(File.dirname(__FILE__), "steep.yml")
   ColumnWidth = 60
   ColorMap = [
     "\033[0;37;40m",
@@ -24,38 +24,76 @@ class Steeper
     }
   
   def initialize
-
-    @steep_duration = 2 # in seconds
     
-    # parsing command line options
-    @options = {}
-    optparse = OptionParser.new do |opts|
-      opts.banner = "Usage: steep.rb [options]"
-      opts.on "-r", "--reset", "Reset steeping count" do |r|
-        @options[:reset] = true
-      end
-      opts.on "-t", "--type TEATYPE", "Type of tea (based on config file #{ConfigFile})" do |t|
-        @options[:type] = t || "green"
-      end
+    # not sure if we need the object returned from optparse or not
+    @optparse = parse_command_line_options
+    @tea_type ||= "green"
+    @using_config_file = true
+    
+    # steep.yml by default
+    load_config_file
+    
+    # validate @tea_type
+    unless @config["teas"].keys.include?(@tea_type)
+      puts "\"#{@tea_type}\" is not available"
+      puts "Available teas: #{@config["teas"].keys.join(", ")}"
+      exit 1
     end
     
+    # setup steeping sequence, defaulting to first steeping
+    @steeping = @reset ? 0 : @config["steepcounter"] || 0
+    
+    
+    durations = @config["teas"][@tea_type]["duration"]
+    
+    # setup steeping duration based on type/number of steepings
+    @steep_duration = durations[@steeping] || durations.last
+    
+  end
+  
+  def parse_command_line_options
+    showhelp = false
+    # parsing command line options
+    optparse = OptionParser.new do |opts|
+      opts.banner = "Usage: steep.rb [options]"
+      opts.on("-r", "--reset", "Reset steeping count") { @reset = true }
+      opts.on("-q", "--quiet", "Suppress all non-essential output") { @quiet =  true }
+      opts.on "-t", "--type TEATYPE", "Type of tea (based on config file #{ConfigFile})" do |t|
+        @tea_type = t
+      end
+      opts.on("-h", "--help", "Show options banner") { |h| showhelp = true  }
+    end.parse!
+    puts optparse
+  end
+  
+  def load_config_file
+    # looking for configuration file
+    # if missing, offers to make one
     if File.readable? ConfigFile
       @config = YAML::load_file(ConfigFile)
     else
       puts "Configuration file #{ConfigFile} missing."
       print "Create config file? (y or n): "
       unless gets.chomp =~ /[nN0]/
-        File.open(File.join(File.dirname(__FILE__), ConfigFile), "w") do |f|
+        File.open(ConfigFile, "w") do |f|
           f.write YAML.dump(DefaultConfigData)
         end
       else
         puts "Fine, be that way!"
+        @using_config_file = false
       end
       @config = DefaultConfigData
       puts "Using defaults for first steeping of green tea"
     end
-    
-    
+  end
+  
+  def update_config_file
+    if @using_config_file
+      configfile = File.open(ConfigFile, "r+")
+      replacement =  configfile.read.gsub(/steepcounter: \d+/, "steepcounter: #{@reset ? 0 : @steeping + 1}")
+      configfile.seek(0)
+      configfile.write(replacement)
+    end
   end
   
   def steep
@@ -69,6 +107,9 @@ class Steeper
       STDOUT.flush
     end
     puts "#{" "*ColumnWidth}\n#{"\007"*3}#{DoneMessage}\033[0m\ndone steeping"
+    update_config_file
+    puts "updating config file"
+    
   end
     
 end
