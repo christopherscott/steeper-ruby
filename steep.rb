4e6e13c5f4d63610929965ac939da8531de6c18d
Chris Hernandez <c.scott.hernandez@gmail.com>
2012-02-02 16:56:06 -0500

    add comments for command line colors

---
+++ steep.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+require 'optparse'
+require 'yaml'
+require './helper'
+
+module Tea
+
+  class Steeper
+    
+    include Tea::Helper
+
+    DoneMessage = IO.read(File.join(File.dirname(__FILE__), "teabag.txt"))  
+    ColumnWidth = 60
+    ColorMap = [
+      "\033[0;37;40m", # light gray
+      "\033[1;30;40m", # dark gray
+      "\033[0;33;40m", # amber
+      "\033[1;33;40m", # yellow
+      "\033[1;32;40m", # light green
+      "\033[0;32;40m"  # dark green
+      ]
+    DefaultConfigData = {
+      "steepcounter" => 0,
+      "teas" => { "green" => { "duration" => [60, 60, 90, 105] } } 
+      }
+    
+    def initialize
+      @optparse = parse_command_line_options
+      @tea_type ||= "green"
+      @using_config_file = true
+      # steep.yml by default
+      load_config_file
+      # validate @tea_type
+      unless @config["teas"].keys.include?(@tea_type)
+        puts "\"#{@tea_type}\" is not available"
+        puts "Available teas: #{@config["teas"].keys.join(", ")}"
+        exit 1
+      end
+      # setup steeping sequence, defaulting to first steeping
+      @steeping = @reset ? 0 : @config["steepcounter"] || 0
+      durations = @config["teas"][@tea_type]["duration"]
+      # setup steeping duration based on type/number of steepings
+      @steep_duration = durations[@steeping] || durations.last
+    end
+    
+    def steep
+      start_time = Time.now.to_f
+      stop_time = start_time + @steep_duration
+      until Time.now.to_f > stop_time
+        percent =  (Time.now.to_f - start_time) / @steep_duration.to_f
+        print "#{"#{ColorMap[percent * 6]}#"*(percent * ColumnWidth)}\r"
+        sleep 1
+        STDOUT.flush
+      end
+      puts "#{" "*ColumnWidth}\n#{"\007"*3}#{DoneMessage}\033[0m\ndone steeping"
+      update_config_file
+      puts "updating config file"
+    end
+      
+  end
+
+end
+
+
+if __FILE__ == $0
+  Tea::Steeper.new.steep
+end
